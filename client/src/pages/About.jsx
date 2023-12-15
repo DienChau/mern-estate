c6c5863b243ed061b1df1b6ed10338c1c17d5335
DienChau <19521275@gm.uit.edu.vn>
2023-12-15 20:55:32 +0700

    complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,21 @@
-
 function About() {
     return (
         <div>
-            About
+            <h1 className="text-3xl sm:text-4xl text-slate-700 text-center mb-10  mt-20 max-w-4xl mx-auto ">
+                Our boutique banquet facility is elegantly designed to host
+                grand weddings, anniversaries, conferences and other special
+                events for up to 200 guests.
+            </h1>
+            <div className="w-[50px] h-[3px] bg-green-400 mx-auto rounded-lg mb-10"></div>
+            <p className="text-sm sm:text-lg font-extralight max-w-[500px] mx-auto leading-relaxed text-center">
+                We offer the most luxurious and extravagant reception options in
+                the area. Our selection of rooms and wide range of menu options
+                make The Estate Room one of the most versatile choices for
+                events and banquets in Northwest Indiana. Our services allow you
+                to truly customize your experience and create lasting memories.
+            </p>
         </div>
-    )
+    );
 }
 
-export default About
+export default About;
